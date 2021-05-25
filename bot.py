d9dd0bbd05970dee49eace9163084d6c7e8b8996
Rexxar <38912563+RexxarCompany@users.noreply.github.com>
2021-05-25 14:32:30 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
+from telegram import Update,ReplyKeyboardMarkup,ReplyKeyboardRemove,Bot,InlineKeyboardButton,InlineKeyboardMarkup,KeyboardButton,CallbackQuery,ParseMode
+from telegram.ext import CommandHandler,Updater,Dispatcher,MessageHandler,Filters,CallbackContext,CallbackQueryHandler
 import logging
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -8,60 +8,50 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+tkn = "1783899723:AAFU9R73sE-Rt2dPcXiQXqjnSeFWiSyKgIU"
+updater = Updater(tkn,use_context=True)
+bot = Bot(tkn)
+dispatcher : Dispatcher = updater.dispatcher
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hey this is your bot!')
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Currently I am in Alpha stage, help me also!')
-
-def piracy(update, context):
-    update.message.reply_text('Ahhan, FBI wants to know your location!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def start(update:Update, context:CallbackContext):
+    firstname = update.effective_message.from_user.first_name
+    chtiD = update.effective_message.chat_id
+    username = update.effective_message.from_user.username
+    txt = update.effective_message.text
+    keyboard = [
+        [KeyboardButton('Help')],
+        [KeyboardButton('Contact us')]
+    ]
+    key = ReplyKeyboardMarkup(keyboard,resize_keyboard=True)
 
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    if txt=="Help":
+        bot.send_message(
+            chat_id=chtiD,
+            text="How to Deploy Your Telegram bot on Heroku\n\nچگونه ربات خود را در Heroku راه اندازی کنید",
+            reply_to_message_id=update.effective_message.message_id,
+        )
+    elif txt=="Contact us":
+        bot.send_message(
+            chat_id=chtiD,
+            text="<u>Website : </u>Rexxar.ir\n\n<i>Telegram : </i>@Rexxar_ir",
+            reply_to_message_id=update.effective_message.message_id,
+            parse_mode=ParseMode.HTML
+        )
+    else:
+        bot.send_message(
+            chat_id=chtiD,
+            text=f"نام کاربری شما {firstname}" + f"\n\nیوزرنیم شما : {username}" + f"\n\nآیدی عددی شما : {str(chtiD)}",
+            reply_to_message_id=update.effective_message.message_id,
+            reply_markup=key
 
+
+        )
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("//TOKEN//", use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("piracy", piracy))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
+    dispatcher.add_handler(MessageHandler(Filters.text,start))
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
 
 
 if __name__ == '__main__':
